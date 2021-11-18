35bf617bedd68aadbe0768ffb434f65f5065daf0
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-18 15:53:35 -0300

    test(cadastro cliente): add teste for CadastroClienteController

---
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
@@ -0,0 +1,7 @@
+describe("Cadastro Cliente Controller", () => {
+    it.todo("should return 400 if no name is provided");
+    it.todo("should return 400 if no email is provided");
+    it.todo("should return 400 if no cpf is provided");
+    it.todo("should return 400 if no telefone is provided");
+    it.todo("should return 400 if no data_nascimento is provided");
+});
