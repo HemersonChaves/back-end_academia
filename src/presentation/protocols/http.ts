f148f65a38b053741f67ae0f7652e4753d74f256
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-18 14:58:27 -0300

    feat(http): add interface IHttpRequest

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,6 +1,19 @@
+/**
+ * interface para respostas das requisições do protocolo http
+ */
 interface IHttpResponse {
+    // Resposta com código dos status do servidor http (220, 400, 500, ...)
     statusCode: number;
     body: any;
 }
 
 export { IHttpResponse };
+/**
+ * interface para requisições dos paramentros do protocolo http
+ */
+interface IHttpRequest {
+    // paramentros enviado via body
+    body?: any;
+}
+
+export { IHttpRequest };
