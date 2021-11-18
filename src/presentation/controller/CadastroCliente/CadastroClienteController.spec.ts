13b915b561a7f06acaa9c860e5cab281b965e54e
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-18 18:03:31 -0300

    test(cadastro client): enruse CadastroClienteController return 400 if no name is provider

--- src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
@@ -1,5 +1,20 @@
+import { CadastroClienteController } from "./CadastroClienteController";
+
 describe("Cadastro Cliente Controller", () => {
-    it.todo("should return 400 if no name is provided");
+    test("should return 400 if no name is provided", async () => {
+        const sysUnderTest = new CadastroClienteController();
+        const httpRequest = {
+            body: {
+                // no name
+                email: "any@email.com",
+                cpf: "00000000000",
+                telefone: "00000000",
+                data_nascimento: "00/00/0000",
+            },
+        };
+        const httpResponse = await sysUnderTest.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
     it.todo("should return 400 if no email is provided");
     it.todo("should return 400 if no cpf is provided");
     it.todo("should return 400 if no telefone is provided");
