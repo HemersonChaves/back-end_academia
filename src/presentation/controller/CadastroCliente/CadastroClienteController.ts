5ea917fe8307f7823a2880d7fb458d7ab49579ca
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-19 15:02:35 -0300

    refactor(cadastro cliente): simplify code require filds

--- src/presentation/controller/CadastroCliente/CadastroClienteController.ts
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.ts
@@ -4,19 +4,14 @@ import { IController } from "@/presentation/protocols/IController";
 
 class CadastroClienteController implements IController {
     handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        console.log(httpRequest.body.name);
-        if (!httpRequest.body.name) {
-            return Promise.resolve({
-                statusCode: 400,
-                body: new MissingParamError("name"),
-            });
-        }
-        if (!httpRequest.body.email) {
-            console.log(httpRequest.body.email);
-            return Promise.resolve({
-                statusCode: 400,
-                body: new MissingParamError("email"),
-            });
+        const requireFields = ["name", "email"];
+        for (const field of requireFields) {
+            if (!httpRequest.body[field]) {
+                return Promise.resolve({
+                    statusCode: 400,
+                    body: new MissingParamError(field),
+                });
+            }
         }
         return Promise.resolve({
             statusCode: 200,
