528e6a43a34546c277c175bbc9a9c9a6726820bf
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-18 22:03:03 -0300

    feat(cadastro cliente): ensure CadastroClienteController return 400 an error if no email is provided

--- src/presentation/controller/CadastroCliente/CadastroClienteController.ts
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.ts
@@ -3,9 +3,23 @@ import { IController } from "@/presentation/protocols/IController";
 
 class CadastroClienteController implements IController {
     handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+        console.log(httpRequest.body.name);
+        if (!httpRequest.body.name) {
+            return Promise.resolve({
+                statusCode: 400,
+                body: new Error("Missing param: name"),
+            });
+        }
+        if (!httpRequest.body.email) {
+            console.log(httpRequest.body.email);
+            return Promise.resolve({
+                statusCode: 400,
+                body: new Error("Missing param: email"),
+            });
+        }
         return Promise.resolve({
-            statusCode: 400,
-            body: new Error("Missing param: name"),
+            statusCode: 200,
+            body: "",
         });
     }
 }
