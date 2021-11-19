0b2ea4d1e49322bf673e458180a6d6e8919947f4
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-18 21:57:21 -0300

    test(cadastro cliente): ensure CadastroClienteController return 400 an error if no email is provided

--- src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
@@ -16,7 +16,21 @@ describe("Cadastro Cliente Controller", () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new Error("Missing param: name"));
     });
-    it.todo("should return 400 if no email is provided");
+    test("should return 400 if no email is provided", async () => {
+        const sysUnderTest = new CadastroClienteController();
+        const httpRequest = {
+            body: {
+                name: "any name",
+                // no email
+                cpf: "00000000000",
+                telefone: "00000000",
+                data_nascimento: "00/00/0000",
+            },
+        };
+        const httpResponse = await sysUnderTest.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+    });
     it.todo("should return 400 if no cpf is provided");
     it.todo("should return 400 if no telefone is provided");
     it.todo("should return 400 if no data_nascimento is provided");
