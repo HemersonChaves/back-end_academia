1da1be0c66701152df22347311f96e40d3692765
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-19 15:08:26 -0300

    test(cadastro cliente): ensure CadastroClienteController return 400 if no cpf

--- src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
+++ src/presentation/controller/CadastroCliente/CadastroClienteController.spec.ts
@@ -36,7 +36,21 @@ describe("Cadastro Cliente Controller", () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError("email"));
     });
-    it.todo("should return 400 if no cpf is provided");
+    test("should return 400 if no cpf is provided", async () => {
+        const sysUnderTest = makeSysUnderTest();
+        const httpRequest = {
+            body: {
+                name: "any name",
+                email: "any@email.com",
+                // no cpf
+                telefone: "00000000",
+                data_nascimento: "00/00/0000",
+            },
+        };
+        const httpResponse = await sysUnderTest.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError("cpf"));
+    });
     it.todo("should return 400 if no telefone is provided");
     it.todo("should return 400 if no data_nascimento is provided");
 });
