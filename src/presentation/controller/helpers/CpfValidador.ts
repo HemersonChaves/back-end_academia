cb2d597407a468446508c7f5aa0074da24fc22c0
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-20 16:19:01 -0300

    feat(validador cpf): ensure return false if cpf is only string or not number

--- src/presentation/controller/helpers/CpfValidador.ts
+++ src/presentation/controller/helpers/CpfValidador.ts
@@ -2,6 +2,18 @@ import { ICpfValidador } from "@/presentation/protocols/ICpfValidador";
 
 class CpfValidador implements ICpfValidador {
     validar(cpf: string) {
+        if (cpf === null || undefined === cpf) {
+            return false;
+        }
+
+        const cpfValido = cpf
+            .replace(".", "")
+            .replace(".", "")
+            .replace("-", "");
+
+        if (cpfValido.length !== 11) {
+            return false;
+        }
         return true;
     }
 }
