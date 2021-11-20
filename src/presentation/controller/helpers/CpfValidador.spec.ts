7445efb6de58afae3102c5cd6a816e4db5c326d3
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-20 16:18:08 -0300

    test(validador cpf): ensure return false if cpf is only string or not number

--- src/presentation/controller/helpers/CpfValidador.spec.ts
+++ src/presentation/controller/helpers/CpfValidador.spec.ts
@@ -1,3 +1,14 @@
-describe("Cpf CpfValidador", () => {
+import { CpfValidador } from "./CpfValidador";
+
+describe("Cpf Validador", () => {
+    test("return false if cpf is only string or not number", () => {
+        const sysUnderTest = new CpfValidador();
+        const cpf = "invalidcpf";
+
+        const validado = sysUnderTest.validar(cpf);
+
+        expect(validado).toBe(false);
+    });
+    it.todo("returns false if cpf is not formated");
     it.todo("should call cpf with correct cpf");
 });
