724e0e924864831ef7d5089602993cc6ab1e52e6
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-22 13:00:48 -0300

    test(validador cpf): ensure not validate format of correct cpf

--- src/presentation/controller/helpers/CpfValidador.spec.ts
+++ src/presentation/controller/helpers/CpfValidador.spec.ts
@@ -3,12 +3,22 @@ import { CpfValidador } from "./CpfValidador";
 describe("Cpf Validador", () => {
     test("return false if cpf is only string or not number", () => {
         const sysUnderTest = new CpfValidador();
-        const cpf = "invalidcpf";
-
-        const validado = sysUnderTest.validar(cpf);
-
-        expect(validado).toBe(false);
+        expect(sysUnderTest.validar("")).toBeFalsy();
+        expect(sysUnderTest.validar("invalidcpf")).toBeFalsy();
     });
-    it.todo("returns false if cpf is not formated");
+    test("return false if cpf is not valided", () => {
+        const sysUnderTest = new CpfValidador();
+        expect(sysUnderTest.validar("00000000000")).toBeFalsy();
+        expect(sysUnderTest.validar("11111111111")).toBeFalsy();
+        expect(sysUnderTest.validar("22222222222")).toBeFalsy();
+        expect(sysUnderTest.validar("33333333333")).toBeFalsy();
+        expect(sysUnderTest.validar("44444444444")).toBeFalsy();
+        expect(sysUnderTest.validar("55555555555")).toBeFalsy();
+        expect(sysUnderTest.validar("66666666666")).toBeFalsy();
+        expect(sysUnderTest.validar("77777777777")).toBeFalsy();
+        expect(sysUnderTest.validar("88888888888")).toBeFalsy();
+        expect(sysUnderTest.validar("99999999999")).toBeFalsy();
+        expect(sysUnderTest.validar("12345678909")).toBeFalsy();
+    });
     it.todo("should call cpf with correct cpf");
 });
