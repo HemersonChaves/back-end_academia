4edb6c99caff58c97094082da729a5744fa237b1
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-22 17:22:50 -0300

    test(validador email): ensure return false if email is not correct

--- src/presentation/controller/helpers/EmailValidador.spec.ts
+++ src/presentation/controller/helpers/EmailValidador.spec.ts
@@ -1,4 +1,16 @@
+import { EmailValidador } from "./EmailValidador";
+
 describe("Email CpfValidador", () => {
-    it.todo("returns false if email is not correct");
+    test("returns false if email is not correct", () => {
+        const sysUnderTest = new EmailValidador();
+        expect(sysUnderTest.Validar("emailinvalido.com")).toBeFalsy();
+        expect(sysUnderTest.Validar("emailinvalido@")).toBeFalsy();
+        expect(
+            sysUnderTest.Validar(
+                "emailinvalido_____________________________________________________@"
+            )
+        ).toBeFalsy();
+        expect(sysUnderTest.Validar("emailinvalido@com")).toBeFalsy();
+    });
     it.todo("should call cpf with correct email");
 });
