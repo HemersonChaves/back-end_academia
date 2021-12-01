2705441f573ccdb61d8195971a3c0249f334762e
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-12-01 13:45:42 -0300

    refactor(controller): add generic type <T = any>

--- src/presentation/protocols/IController.ts
+++ src/presentation/protocols/IController.ts
@@ -1,9 +1,9 @@
-import { IHttpRequest, IHttpResponse } from "./http";
+import { IHttpResponse } from "./http";
 /**
  * Interface para disparo de solicitações http
  */
-interface IController {
-    handle(httpRequest: IHttpRequest): Promise<IHttpResponse>;
+interface IController<T = any> {
+    handle(httpRequest: T): Promise<IHttpResponse>;
 }
 
 export { IController };
