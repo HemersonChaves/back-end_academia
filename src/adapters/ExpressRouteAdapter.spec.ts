68ae532b46e6c6fbe36a6e7c192279d063cc4ce4
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-12-07 18:16:50 -0300

    test(adapter route): enruse respond with 400 and 500  and valid error

--- src/adapters/ExpressRouteAdapter.spec.ts
+++ src/adapters/ExpressRouteAdapter.spec.ts
@@ -81,4 +81,38 @@ describe("Adapter Router Express", () => {
         expect(retorno.json).toHaveBeenCalledWith(null);
         expect(retorno.status).toHaveBeenCalledTimes(1);
     });
+    test("should respond with 400 and valid error", async () => {
+        const controllerSpy = new ControllerSpy();
+        jest.spyOn(controllerSpy, "handle").mockResolvedValueOnce({
+            statusCode: 400,
+            body: new Error("any_error"),
+        });
+        const SysUnderTest = new AdapterRoute(controllerSpy);
+        const request = getMockReq({
+            data: "any",
+        });
+        const response = getMockRes().res;
+        const retorno = await SysUnderTest.handle(request, response);
+        expect(retorno.status).toHaveBeenCalledWith(400);
+        expect(controllerSpy.handle).toHaveBeenCalledTimes(1);
+        expect(retorno.json).toHaveBeenCalledWith({ error: "any_error" });
+        expect(retorno.status).toHaveBeenCalledTimes(1);
+    });
+    test("should respond with 400 and valid error", async () => {
+        const controllerSpy = new ControllerSpy();
+        jest.spyOn(controllerSpy, "handle").mockResolvedValueOnce({
+            statusCode: 500,
+            body: new Error("any_error"),
+        });
+        const SysUnderTest = new AdapterRoute(controllerSpy);
+        const request = getMockReq({
+            data: "any",
+        });
+        const response = getMockRes().res;
+        const retorno = await SysUnderTest.handle(request, response);
+        expect(retorno.status).toHaveBeenCalledWith(500);
+        expect(controllerSpy.handle).toHaveBeenCalledTimes(1);
+        expect(retorno.json).toHaveBeenCalledWith({ error: "any_error" });
+        expect(retorno.status).toHaveBeenCalledTimes(1);
+    });
 });
