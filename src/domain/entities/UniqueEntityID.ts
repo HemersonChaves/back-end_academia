8168b1064b99918eca0ad517e985ca2a39d8706c
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-01-04 16:14:58 -0300

    refactor(unique id): refactor UniqueEntityID to abstract class

--- src/domain/entities/UniqueEntityID.ts
+++ src/domain/entities/UniqueEntityID.ts
@@ -1,7 +1,29 @@
-class UniqueEntityID<T> {
-    private readonly id: T;
-    constructor(id: T) {
-        this.id = id;
-    }
+interface IUniqueEntityID<T> {
+    generateID(): T;
 }
-export { UniqueEntityID };
+abstract class UniqueEntityID<T> implements IUniqueEntityID<T> {
+    protected value: T;
+    abstract generateID(): T;
+    constructor(value: T) {
+        this.value = value;
+    }
+    // public equals(id?: UniqueEntityID): boolean {
+    //     if (!id || !UniqueEntityID.isIdentifier(id)) {
+    //         return false;
+    //     }
+
+    //     return id.toValue() === this.value;
+    // }
+
+    // public toString(): string {
+    //     return String(this.value);
+    // }
+
+    public toValue(): T {
+        return this.value;
+    }
+    // public static isIdentifier = (id: unknown): id is UniqueEntityID => {
+    //     return id instanceof UniqueEntityID;
+    // };
+}
+export { IUniqueEntityID, UniqueEntityID };
