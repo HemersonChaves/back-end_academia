7bf678b42fe0fc6dff8f8438fe18f34d3dfe8ff4
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-01-04 16:35:08 -0300

    test(unique id): ensure return casting toString UniqueEntityID

--- src/domain/entities/UniqueEntityID.spec.ts
+++ src/domain/entities/UniqueEntityID.spec.ts
@@ -16,8 +16,18 @@ describe("UniqueEntityID", () => {
     test("should UniqueEntityID instanced with number params", () => {
         const idNumber = 123;
         const SysUnderTest = new UniqueIDIntConcretMock(idNumber);
+
         expect(SysUnderTest).toBeInstanceOf(UniqueEntityID);
         expect(SysUnderTest).toBeTruthy();
         expect(SysUnderTest.toValue()).toBe(idNumber);
+    });
+    test("should return casting toString UniqueEntityID", () => {
+        class UniqueIDIntConcret extends UniqueEntityID<number> {
+            generateID(): number {
+                return 123;
+            }
+        }
+        const SysUnderTest = new UniqueIDIntConcret(123);
+        expect(SysUnderTest.toString()).toBe("123");
     });
 });
